37010a0c672cec38f2cc93127032952300e1a724
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2023-02-07 18:39:40 -0500

    final commit

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function changeStyle() {
+	// change header from italic to normal
 	if (document.getElementById("header").style.fontStyle == "italic") {
 		document.getElementById("header").style.fontStyle = "normal";
 	}
 
+	// change header to italic
 	else {
 		document.getElementById("header").style.fontStyle = "italic";
 	}
